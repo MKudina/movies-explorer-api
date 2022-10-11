83906deea10ec496fdba383961a7beed702626ea
Maksim Kudina <KudinaMD@yandex.ru>
2022-10-12 00:34:29 +0300

    feature: add backend

---
+++ models/movie.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const { isURL } = require('validator');
+
+const movieSchema = mongoose.Schema({
+  contry: {
+    type: String,
+    require: true,
+  },
+  director: {
+    type: String,
+    require: true,
+  },
+  duration: {
+    type: Number,
+    require: true,
+  },
+  year: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+    validate: isURL,
+  },
+  trailerLink: {
+    type: String,
+    require: true,
+    validate: isURL,
+  },
+  thumbnail: {
+    type: String,
+    require: true,
+    validate: isURL,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+  },
+  nameRU: {
+    type: String,
+    require: true,
+  },
+  nameEN: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcryptjs');
+const Unauthorized = require('../utils/Unauthorized');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    validate: isEmail,
+  },
+  password: {
+    type: String,
+    require: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    require: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+
+});
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
